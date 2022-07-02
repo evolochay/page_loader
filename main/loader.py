cfd2e854fb93d6759805f44218bb194b50f8a993
volochay <evgenia@volochay.info>
2022-07-02 13:50:14 +0200

    2nd step in progress

--- main/loader.py
+++ main/loader.py
@@ -1,24 +1,74 @@
 import requests
 import re
 import os
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
 
 
 def download(dir_name, page_adress):
-    text_from_url = get_page(page_adress).text
-    file_name = create_file_name(page_adress)
+    #main page download
+    page_for_saving = requests.get(page_adress)
+    soup = BeautifulSoup(page_for_saving.text, 'html.parser')
+
+    #create dir for files
+    resources_dir = create_name(page_adress, 'directory')
+    files_dir_path = os.path.join(dir_name+'/', resources_dir)
+    create_dir(files_dir_path)
+    
+    #download image
+    domen_name = find_domen_name(page_adress)
+    image_url = domen_name + find_image_path(soup)
+    image_name = create_name(image_url, 'image')
+    # image_url https://mikhailovsky.ru/upload/resize_cache/iblock/3f2/336_336_1/kurenkova_ekaterina.jpg
+    image_link = download_image(image_url, image_name, files_dir_path)
+
+
+    page_name = create_name(page_adress, 'page')
     if os.path.isdir(dir_name):
-        file_full_path = os.path.join(dir_name+'/', file_name)
+        file_full_path = os.path.join(dir_name+'/', page_name)
         with open(file_full_path, 'w') as new_file:
-            new_file.write(text_from_url)
+            new_file.write(soup.prettify())
         return file_full_path
 
 
-def get_page(page_url):
-    downloaded_page = requests.get(page_url)
-    return downloaded_page
+def create_name(some_url, type):    
+    without_scheme = some_url[some_url.find('//') + 2:]
+    get_extension = os.path.splitext(some_url)
+    if type == 'image':
+        name = re.sub(r'[^a-zA-Z0-9]', '-', without_scheme[:-4]) + get_extension[1]
+    elif type == 'page':
+        name = re.sub(r'[^a-zA-Z0-9]', '-', without_scheme) + '.html'
+    elif type == 'directory':
+        name = re.sub(r'[^a-zA-Z0-9]', '-', without_scheme[:-1]) + '_files'
+    return name
 
 
-def create_file_name(page_adress):
-    without_scheme = page_adress[page_adress.find('//') + 2:]
-    file_name = re.sub(r'[^a-zA-Z0-9]', '-', without_scheme) + '.html'
-    return file_name
+def find_image_path(soup):
+    resource_dict = {'img': 'src'}
+    images = soup.find_all(resource_dict)
+    for image in images:
+        return(image['src'])
+
+
+def find_domen_name(url):
+    full_adress = urlparse(url)
+    return (full_adress.scheme+"://"+full_adress.netloc)
+
+
+def download_image(image_url, image_name, dir_path):
+    print('URL {}'.format(image_url))
+    print('NAME {}'.format(image_name))
+    print('DIR {}'.format(dir_path))
+
+    response = requests.get(image_url)
+    if response.ok:
+        full_path = os.path.join(dir_path+'/', image_name)
+        with open(full_path, 'wb') as f:
+            f.write(response.content)
+        return full_path
+
+
+def create_dir(path):
+   # head = os.path.split(url)[0]
+    if not os.path.exists(path):
+        os.mkdir(path)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
-import os
 from tempfile import TemporaryDirectory
-from main.loader import download, get_page, create_file_name
+from main.loader import download, create_name
 import requests_mock
+from bs4 import BeautifulSoup
 
 
 URL_COURSES = 'https://ru.hexlet.io/courses'
-URL_Q = "https://ru.hexlet.io/qna"
+URL_Q = "https://quotes.toscrape.com"
 EXPECTED_FILE_NAME = 'ru-hexlet-io-courses.html'
 EXPECTED_HTML = 'tests/fixtures/expected.html'
 
@@ -18,7 +18,7 @@ def read_file(file_path):
 
 
 def test_create_file_name():
-    result = create_file_name(URL_COURSES)
+    result = create_name(URL_COURSES)
     assert result == EXPECTED_FILE_NAME
 
 
@@ -26,7 +26,8 @@ def test_dowloads():
     with TemporaryDirectory() as test_dir:
         with requests_mock.Mocker() as mock:
             test_file = read_file(EXPECTED_HTML)
-            mock.get(URL_Q, text = test_file)
+            soup = BeautifulSoup(test_file, 'html.parser')
+            mock.get(URL_Q, text = str(soup))
             html_file_path = download(test_dir, URL_Q)
             file1 = read_file(html_file_path)
             assert test_file == file1
