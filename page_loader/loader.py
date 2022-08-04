7a2fe8b1205bf2c542ae17ef7eb7477a8dd834da
volochay <evgenia@volochay.info>
2022-08-04 22:07:30 +0200

    problem with sysexit maybe fixed

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -13,11 +13,13 @@ def download(page_adress, destination_dir_name):
     logger.info(f"requested url: {page_adress}")
     logger.info(
         f"output path:{os.path.join(os.getcwd(), destination_dir_name)}")
-    dir_for_files = create_dir(destination_dir_name, page_adress)
-    soup = create_soup(page_adress)
-    save_files(soup, dir_for_files, page_adress)
-    file_full_path = create_html(soup, page_adress, destination_dir_name)
-
+    try:
+        dir_for_files = create_dir(destination_dir_name, page_adress)
+        soup = create_soup(page_adress)
+        save_files(soup, dir_for_files, page_adress)
+        file_full_path = create_html(soup, page_adress, destination_dir_name)
+    except Exception:
+        raise
     return file_full_path
 
 
