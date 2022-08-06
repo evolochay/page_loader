8dc9dfe16c72903c6e039376857e1e1ba13ef791
volochay <evgenia@volochay.info>
2022-08-06 23:01:14 +0200

    one more try to fix Hexlet`s check problem

--- page_loader/scripts/loader.py
+++ page_loader/scripts/loader.py
@@ -3,6 +3,7 @@ import os
 import sys
 from page_loader.loader import download
 from logs.log_config import init_logger
+import requests
 
 
 logger = init_logger('app')
@@ -28,11 +29,37 @@ def main():
     try:
         downloaded_page = download(args.page_adress, args.output)
         print(downloaded_page)
-        sys.exit(0)
-    except Exception:
+
+    except FileNotFoundError as fnfe:
+        print(fnfe)
         sys.exit(1)
-    except FileExistsError:
-        sys.exit(2)
+
+    except PermissionError as pe:
+        print(pe)
+        sys.exit(1)
+
+    except NotADirectoryError as nade:
+        print(nade)
+        sys.exit(1)
+
+    except requests.exceptions.ConnectionError:
+        print(f'Unable to connect to {args.page_adress}')
+        sys.exit(1)
+
+    except ConnectionAbortedError as cae:
+        print(cae)
+        sys.exit(1)
+
+    except FileExistsError as fee:
+        print(fee)
+        sys.exit(1)
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+    else:
+        sys.exit(0)
 
 
 if __name__ == '__main__':
