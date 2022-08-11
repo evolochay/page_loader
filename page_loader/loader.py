d61b2e22fda41cadb71a93af2ed5146814e01e66
volochay <evgenia@volochay.info>
2022-08-11 21:11:18 +0200

    trying to solve problem with file`s name

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -13,6 +13,7 @@ def download(page_url, destination_dir_name):
         dir_for_files = create_dir(destination_dir_name, page_url)
         content = save_files(page_path, dir_for_files, page_url)
         download_content(content, destination_dir_name)
+        logger.debug('Here is final PATH {}'.format(page_path))
         return page_path
     except Exception:
         shutil.rmtree(dir_for_files)
