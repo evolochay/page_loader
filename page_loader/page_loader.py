3ec63ec8782c31ac5a40b948a7570663b6fa2d03
volochay <evgenia@volochay.info>
2022-09-17 22:22:51 +0200

    try to find right url to dowload

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -1,8 +1,7 @@
 import os
-from page_loader.page import download_page, get_soup
+from page_loader.page import download_page
 from page_loader.directory import create_dir, dir_validation
-from page_loader.work_with_content import download_content, find_content
-from page_loader.naming import make_clear_url
+from page_loader.work_with_content import download_content
 from logs.log_config import logger
 
 
@@ -14,9 +13,6 @@ def download(page_url, destination_dir_name):
     checked_dir = dir_validation(destination_dir_name)
     page_path = download_page(page_url, checked_dir)
     dir_name, dir_path = create_dir(checked_dir, page_url)
-    soup = get_soup(page_path)
-    content = find_content(soup, page_url)
-    clear_url = make_clear_url(page_url)
-    download_content(content, page_path, dir_path, dir_name, soup, clear_url)
+    download_content(page_url, page_path, dir_path, dir_name)
     logger.debug("Here is final PATH {}".format(page_path))
     return page_path
--- page_loader/work_with_content.py
+++ page_loader/work_with_content.py
@@ -2,21 +2,26 @@ from asyncio.log import logger
 import requests
 from urllib.parse import urljoin, urlparse
 from progress.bar import Bar
-from page_loader.naming import create_name
+from page_loader.naming import create_name, make_clear_url
 from page_loader.directory import make_path
-from page_loader.page import writing, make_url_request
+from page_loader.page import writing, make_url_request, get_soup
 
 TAGS = {"img": "src", "link": "href", "script": "src"}
 
 
-def download_content(resource_dict, page_path, dir_path, dir_name, soup, clear_url):
-    count = len(resource_dict)
+def download_content(page_url, page_path, dir_path, dir_name):
+    soup = get_soup(page_path)
+    resources = find_content(soup, page_url)
+    logger.info("page url {}".format(page_url))
+    clear_url = make_clear_url(page_url)
+    logger.info("clear url {}".format(clear_url))
+    count = len(resources)
     logger.info('page path{}'.format(page_path))
     logger.info('dir path{}'.format(dir_path))
     logger.info('dir name{}'.format(dir_name))
     logger.info('I will download {} content links'.format(count))
     with Bar("Processing", max=count) as bar:
-        for res in resource_dict:
+        for res in resources:
             source_atr = TAGS[res.name]
             res_url = urljoin(page_path, res[source_atr])
             res_name = create_name(res_url, "file")
